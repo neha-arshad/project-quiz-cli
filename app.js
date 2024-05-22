ec9e79989dcea55a49728ad3eb3f58406d4aca54
neha-arshad <studentai345@gmail.com>
2024-05-22 14:19:46 -0700

    complete

--- app.js
+++ app.js
@@ -1,52 +1,50 @@
 #! /usr/bin/env node
 import inquirer from 'inquirer';
 import chalk from "chalk";
-// // DYNAMIC
-let correctCount = 0;
-let incorrectCount = 0;
-// Array of questions
-const questions = [
-    {
-        message: "When did Pakistan come into being?",
-        choices: ["1947", "1965", "1782", "2004"],
-        correctAnswer: "1947"
-    },
-    {
-        message: "Which language is TypeScript compiled to?",
-        choices: ["C#", "React.js", "JavaScript", "HTML"],
-        correctAnswer: "JavaScript"
-    },
-    {
-        message: "Which language is used to styling?",
-        choices: ["C#", "CSS", "JavaScript", "HTML"],
-        correctAnswer: "CSS"
-    },
-];
-// for Loop through each question
-for (const question of questions) {
-    const userAnswer = await inquirer.prompt([
-        {
-            name: "question", // Dynamically generate name for each question
-            type: "list",
-            message: question.message,
-            choices: question.choices
+import chalkanimation from "chalk-animation";
+const sleep = async (ms = 2000) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+};
+const wellcome = chalkanimation.rainbow("\t✨✨ WELCOME TO THE QUIZ..\n");
+await sleep();
+wellcome.stop();
+//console.log(chalk.italic.bold.blueBright("\t✨✨ WELCOME TO THE QUIZ..\n"));
+console.log(chalk.bold.italic.magentaBright("Please answer the following questions..\n"));
+console.log(chalk.bold.italic.blueBright("\tYou will be given 5 questions..\n"));
+console.log(chalk.bold.italic.magentaBright("\tYou will be given 4 options..\n"));
+const apiLink = "https://opentdb.com/api.php?amount=6&category=18&difficulty=easy&type=multiple";
+let fetchData = async (data) => {
+    let fetchQuiz = await fetch(data);
+    let quizData = await fetchQuiz.json();
+    return quizData.results;
+};
+let data = await fetchData(apiLink);
+//console.log(data.result);
+let startQuiz = async () => {
+    let score = 0;
+    let name = await inquirer.prompt({
+        name: "name",
+        type: "input",
+        message: "Enter your name"
+    });
+    for (let i = 1; i <= 5; i++) {
+        let answers = [...data[i].incorrect_answers, data[i].correct_answer];
+        let ans = await inquirer.prompt([
+            {
+                name: "ans",
+                type: "list",
+                message: data[i].question,
+                choices: answers.map((val) => val),
+            },
+        ]);
+        if (ans.ans == data[i].correct_answer) {
+            score++;
+            console.log(chalk.italic.bold.bgMagentaBright("correct  ✅\n"));
         }
-    ]);
-    // Check if user's answer is correct
-    if (userAnswer[`question`] === question.correctAnswer) {
-        console.log(chalk.italic.bgBlueBright(`correct✅\n`));
-        correctCount++;
+        else {
+            console.log(chalk.italic.bold.bgBlueBright("incorrect  ❌\n"));
+        }
     }
-    else {
-        console.log(chalk.italic.bgBlue(`incorrect❌`));
-        incorrectCount++;
-    }
-}
-console.log(chalk.bold.bgMagentaBright(`Your Correct ✅ Answers: ${correctCount}...\n`));
-console.log(chalk.bold.red(` Your Incorrect ❌ Answers: ${incorrectCount}...`));
-if (incorrectCount < correctCount) {
-    console.log(chalk.italic.blueBright("congratulations🎉🥳 .. you pass the quiz"));
-}
-else {
-    console.log(chalk.red("Oppss😥..you don't qualified quiz"));
-}
+    console.log(`Dear ${chalk.italic.bold.blueBright(name.name)}, your correct answer is ${chalk.bold.red(score)}, out of ${chalk.bold.red("5")}`);
+};
+startQuiz();
